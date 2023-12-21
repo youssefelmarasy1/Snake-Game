b4ca0fb6583ca0b21f6525859ea7ccc49a8d1912
Youssef Elmarasy <yelmaras@ualberta.ca>
2023-12-21 08:48:12 -0700

    Frame building functions complete

--- snake.cpp
+++ snake.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include <stdio.h>
 using namespace std;
 
 // Boolean variable to indicate whether or not the game is over
@@ -9,7 +8,8 @@ bool game_over;
 bool new_game;
 
 // This is for the boundary size
-const int height, width = 20;
+const int width = 20;
+const int height = 20;
 
 // These are for the x and y cooridnates of the snake's head, and fruit resepctively
 int x, y, x_fruit, y_fruit;
@@ -32,8 +32,8 @@ bool Setup() {
     x_fruit = rand() % width;
     y_fruit = rand() % height;
     score = 0;
-    new_game = true;
-    return new_game
+    new_game = false;
+    return new_game;
 }
 
 void Draw(bool is_game_starting) {
@@ -42,15 +42,34 @@ void Draw(bool is_game_starting) {
         system("cls");
     }
     // This for loop is for drawing the top wall
-    for (int i =0; i <= width; i++){
+    for (int i =0; i < width +2; i++){
         cout << "#";
     }
+    cout << endl;
+
+    // This for loop is for drawing the left and right walls
+    for (int i =0; i < height; i++){
+
+        for (int j = 0; j < width; j++){
+
+            if (j == 0){
+                cout << "#";
+            }
+            cout << " ";
+            if (j == width-1){
+                cout << "#";
+            }
+        }
+        cout << endl;
+    }
     // This for loop is for drawing the bottom wall
-    for (int i =0; i <= width; i++){
+    for (int i =0; i < width +2; i++){
         cout << "#";
     }
 }
 int main() {
-    cout << "Hello World!" << endl;
+    new_game = Setup();
+    Draw(new_game);
+
     return 0;
 }
